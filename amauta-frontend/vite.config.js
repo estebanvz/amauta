7cf36ae205e5006273fec212774bfece738b8bdf
esteban vilca <esteban.wilfredo.g@gmail.com>
2022-09-30 02:52:44 +0000

    FIX ico image

--- amauta-frontend/vite.config.js
+++ amauta-frontend/vite.config.js
@@ -4,6 +4,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://estebanvz.github.io/amauta/",
   plugins: [react()],
   define: {
     "global": "window",
